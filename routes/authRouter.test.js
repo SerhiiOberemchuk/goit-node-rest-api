9ba63e6b0a1c7adb2bb7ea5ffed3b02a4cb326ae
SerhiiOberemchuk <gardenitparts@gmail.com>
2024-03-23 21:51:37 +0100

    test

--- routes/authRouter.test.js
+++ routes/authRouter.test.js
@@ -7,23 +7,19 @@ const { PORT = 3000, DB_HOST } = process.env;
 
 describe("test api/users/login function", () => {
   let server = null;
-
   beforeAll(async () => {
     await mongoose.connect(DB_HOST);
     server = app.listen(PORT);
   });
-
   afterAll(async () => {
     await mongoose.connection.close();
     server.close();
   });
-
   test("test /login with correct data", async () => {
     const loginData = {
       email: "serhii@i.ua",
       password: "123456",
     };
-
     const { statusCode, body } = await request(app)
       .post("/api/users/login")
       .send(loginData);
@@ -31,9 +27,7 @@ describe("test api/users/login function", () => {
     expect(statusCode).toBe(200);
     expect(body).toHaveProperty("token");
     expect(body).toHaveProperty("user");
-
     const { user } = body;
-
     expect(typeof user.email).toBe("string");
     expect(typeof user.subscription).toBe("string");
   });
